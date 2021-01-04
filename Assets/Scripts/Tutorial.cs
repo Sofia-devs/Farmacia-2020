86f24fbd696778cf6580cfdd04bb1417b90b3ff5
Alvaritroker <alvaritroker@users.noreply.github.com>
2021-01-04 13:47:33 +0100

    tutorial remaked

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -11,17 +11,7 @@ public class Tutorial : MonoBehaviour
     public Text dialogText;
     public bool borroso;
 
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void ActivarTutorial()
     {
         Invoke("AvatarAparece", 2);
@@ -33,106 +23,115 @@ public class Tutorial : MonoBehaviour
     {
         panelAvatar.SetActive(true);
         panelAvatar.transform.GetChild(0).gameObject.SetActive(true); //activar borroso
-
         Invoke("Dialog1", 0.5f);
-
     }
-
-    public void Dialog1()
-    {
-        faseTutorial = 0;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
-
-        Invoke("Dialog2", 0.5f);
-
-    }
-
-    public void Dialog2()
+    public void PassDialog()
     {
         faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
-        Invoke("Dialog3", 0.5f);
-
     }
 
-    public void Dialog3()
+    public void Dialog(int phase)
     {
-        faseTutorial++;
+        faseTutorial = phase;
         dialogText.text = dialogPhraseTutorial[faseTutorial];
-        Invoke("Dialog4", 0.5f);
-
+        Invoke("PassDialog", 0.5f);
     }
 
-    public void Dialog4()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //public void Dialog1()
+    //{
+    //    faseTutorial = 0;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
 
-        Invoke("Dialog5", 0.5f);
+    //    Invoke("Dialog2", 0.5f);
 
-    }
+    //}
 
-    public void Dialog5()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //public void Dialog2()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //    Invoke("Dialog3", 0.5f);
 
-        Invoke("Dialog6", 0.5f);
+    //}
 
-    }
+    //public void Dialog3()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //    Invoke("Dialog4", 0.5f);
 
-    public void Dialog6()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //}
 
-        Invoke("Dialog7", 0.5f);
+    //public void Dialog4()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
 
-    }
+    //    Invoke("Dialog5", 0.5f);
 
-    public void Dialog7()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //}
 
-        Invoke("Dialog8", 0.5f);
+    //public void Dialog5()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
 
-    }
+    //    Invoke("Dialog6", 0.5f);
 
-    public void Dialog8()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //}
 
-        Invoke("Dialog9", 0.5f);
+    //public void Dialog6()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
 
-    }
+    //    Invoke("Dialog7", 0.5f);
 
-    public void Dialog9()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //}
 
-        Invoke("Dialog10", 0.5f);
+    //public void Dialog7()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
 
-    }
+    //    Invoke("Dialog8", 0.5f);
 
-    public void Dialog10()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //}
 
-        Invoke("Dialog11", 0.5f);
+    //public void Dialog8()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
 
-    }
+    //    Invoke("Dialog9", 0.5f);
 
-    public void Dialog11()
-    {
-        faseTutorial++;
-        dialogText.text = dialogPhraseTutorial[faseTutorial];
+    //}
 
-        Invoke("Dialog12", 0.5f);
+    //public void Dialog9()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
 
-    }
+    //    Invoke("Dialog10", 0.5f);
+
+    //}
+
+    //public void Dialog10()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
+
+    //    Invoke("Dialog11", 0.5f);
+
+    //}
+
+    //public void Dialog11()
+    //{
+    //    faseTutorial++;
+    //    dialogText.text = dialogPhraseTutorial[faseTutorial];
+
+    //    Invoke("Dialog12", 0.5f);
+
+    //}
 
 }
