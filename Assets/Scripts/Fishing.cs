3fe121279e275c784053cbbec2a4e5677827a7d7
Alvaritroker <alvaritroker@users.noreply.github.com>
2021-01-07 14:17:44 +0100

    Minigame Mechanics

--- Assets/Scripts/Fishing.cs
+++ Assets/Scripts/Fishing.cs
@@ -5,29 +5,55 @@ using UnityEngine;
 public class Fishing : MonoBehaviour
 {
     public GameObject fishingArea;
+    FollowthePath pathSc;
     GameObject fisherman;
     public float cooldown;
+    public List<GameObject> waypointsCheck;
+
     // Start is called before the first frame update
     void Start()
     {
         fisherman = gameObject;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
 
 
     public void Fish()
     {
         fishingArea.SetActive(true);
-        Invoke("NetUp", cooldown);
+        waypointsCheck.AddRange(GameObject.FindGameObjectsWithTag("Angio"));
+        waypointsCheck.AddRange(GameObject.FindGameObjectsWithTag("BadResource"));
+        foreach (GameObject waypoints in waypointsCheck)
+        {
+            if(waypoints.GetComponent<FollowthePath>().waypointIndex == 2)
+            {
+                print("pescar");
+                waypoints.GetComponent<FollowthePath>().gotocesta = true;
+                pathSc = waypoints.GetComponent<FollowthePath>();
+            }
+        }
+
+        //Invoke("NetUp", cooldown);
+        NetUp();
     }
 
     void NetUp()
     {
+        if(pathSc != null)
+        {
+            if (pathSc.Destruir() == "Angio")
+            {
+                print("angio");
+            }
+        }
+
         fishingArea.SetActive(false);
     }
 }
--- Assets/Scripts/FollowthePath.cs
+++ Assets/Scripts/FollowthePath.cs
@@ -7,13 +7,13 @@ public class FollowthePath : MonoBehaviour
     //Arry of waypoints to walk from one to the next one
     [SerializeField]
     private Transform[] waypoints;
-    private Transform cesta;
+    public Transform cesta;
     //Walk speed
     [SerializeField]
     private float moveSpeed = 2f;
 
     //Index of current waypoint from which boat walks to the next
-    private int waypointIndex = 0;
+    public int waypointIndex = 0;
 
     public bool activarBoat = false;
 
@@ -43,6 +43,10 @@ public class FollowthePath : MonoBehaviour
         {
             if (!gotocesta)
             {
+                if(moveSpeed != 2)
+                {
+                    moveSpeed = 2;
+                }
                 //Move boat from current waypoint to the next one using MoveTowards method
                 transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
 
@@ -53,14 +57,37 @@ public class FollowthePath : MonoBehaviour
                     if(waypointIndex == waypoints.Length)
                     {
                         waypointIndex = 0;
+                        transform.position = waypoints[waypointIndex].transform.position;
+
                     }
                 }
             }
             else
             {
+                print("cesta");
+                if (moveSpeed != 5)
+                {
+                    moveSpeed = 5;
+                }
                 transform.position = Vector2.MoveTowards(transform.position, cesta.position, moveSpeed * Time.deltaTime);
-                waypointIndex = 0;
+                //waypointIndex = 0;
+                if(transform.position == cesta.position)
+                {
+                    Invoke("Restart", 1);
+                }
             }
         }
+    }
+
+    public string Destruir()
+    {
+        return (gameObject.tag);
+    }
+
+    void Restart()
+    {
+        waypointIndex = 0;
+        gotocesta = false;
+        transform.position = waypoints [waypointIndex].transform.position;
     }
 }
